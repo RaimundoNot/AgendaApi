82269a628e5785aba36efbd13e744b0291341702
RaimundoNot <raimundomotadeol@gmail.com>
2026-01-28 14:34:11 -0400

    Final

--- obj/Debug/net10.0/ModuloAPI.AssemblyInfo.cs
+++ obj/Debug/net10.0/ModuloAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e48488d838387e83d9e11d4be4bab091ee5a568")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9623c59cdedfd8b83f9c3eeb0630f37f5d65114f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ModuloAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
